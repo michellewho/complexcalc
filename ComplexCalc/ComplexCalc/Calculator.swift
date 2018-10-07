a276678a709c58dd91488f2340526cc84ad5e155
Michelle <michho@uw.edu>
2018-10-07 02:21:57 -0700

    all tests passed

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,79 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ arr: [Int]) -> Int {
+        var sum = 0
+        for each in arr {
+            sum = sum + each
+        }
+        return sum
+    }
+    
+    func multiply(_ arr: [Int]) -> Int {
+        var product = 1
+        for each in arr {
+            product = product * each
+        }
+        return product
+    }
+    
+    func count(_ arr: [Int]) -> Int {
+        return arr.count
+    }
+    
+    func avg(_ arr: [Int]) -> Int {
+        var sum = self.add(arr)
+        return sum / arr.count
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var product = 0
+        var start = beg
+        
+        for each in args {
+            product = op(start, each)
+            start = product
+        }
+        return product
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        let x = lhs["x"]! + rhs["x"]!
+        let y = lhs["y"]! + rhs["y"]!
+        return ["x": x, "y": y]
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        let x = lhs["x"]! - rhs["x"]!
+        let y = lhs["y"]! - rhs["y"]!
+        return ["x": x, "y": y]
+    }
 }
